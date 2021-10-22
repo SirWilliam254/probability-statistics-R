a446134b18b13accae5a0f75ed55048475a9f662
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2021-10-23 00:07:09 +0300

    Add files via upload

---
+++ combinations.R
@@ -0,0 +1,11 @@
+library(gtools) # loading required package
+
+objectsz <- letters[1:13]
+choose(13, 3) # nCr....i.e 5C2
+objectz.combins <- combinations(13, 3, v=objectz, set=TRUE, repeats.allowed=FALSE)
+objectz.combins
+
+
+# Combination with Replacement
+factorial(n+r-1)/ (factorial(n-1)*factorial(r))
+
