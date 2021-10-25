2134d3a10b659f7ac88dc827f1a97d711c5ee957
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2021-10-25 18:39:02 +0300

    Add files via upload

---
+++ geometric.R
@@ -0,0 +1,12 @@
+#dgeom(x, p)
+#pgeom(x, p)
+#qgeom(Fx, p)
+#rgeom(numSims, p)
+
+# probability of observing 5 failures before the first success, given the 
+# probability of success is 0.3.
+p <- 0.2
+x <- 5
+dgeom(x, p)
+ # alternatively
+p*(1-p)^x
