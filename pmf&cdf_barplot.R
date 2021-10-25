4bfcb9e98d6855adb55f316f1f2833d6909c0a6a
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2021-10-25 12:17:11 +0300

    Update pmf&cdf_barplot.R

--- pmf&cdf_barplot.R
+++ pmf&cdf_barplot.R
@@ -1,5 +1,6 @@
-# Finding and graphing the pmf of X.
-px <- c((9/12)*(8/11), (9/12)*(3/11) + (3/12)*(9/11), (3/12)*(2/11)) # pmf of x
+# Two coins are flipped. Letting X denote the number of heads observed.
+# Finding and graphing the pmf and cdf of X:
+px <- c(0.25, 0.5, 0.25) # pmf of x
 MASS::fractions(px)
 px
 barplot(px, names.arg=0:2, ylim=c(0,1), xlab="x", ylab="p(x)") #bar plot
@@ -9,6 +10,6 @@ barplot(px, names.arg=0:2, ylim=c(0,1), xlab="x", ylab="p(x)") #bar plot
 # we get the cumulative probabilities px
 cumsum(px)
 # plotting from zero
-sfun0 <- stepfun(0:2, c(0, cumsum(px)), f = 0)
-plot(sfun0, verticals=FALSE, xlim=c(0, 3), pch = 19, main=NULL,
+cdf_0 <- stepfun(0:2, c(0, cumsum(px)), f = 0)
+plot(cdf_0, verticals=FALSE, xlim=c(0, 3), pch = 19, main=NULL,
      ylab="F(x)")
