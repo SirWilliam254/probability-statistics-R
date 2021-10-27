74cc5e802a48a8a74259829951f22f5a796237bc
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2021-10-27 09:26:45 +0300

    Update combinations.R

--- combinations.R
+++ combinations.R
@@ -1,8 +1,8 @@
 library(gtools) # loading required package
 
-objectsz <- letters[1:13]
+objectz <- letters[1:13]
 choose(13, 3) # nCr....i.e 5C2
-objectz.combins <- combinations(13, 3, v=objectz, set=TRUE, repeats.allowed=FALSE)
+objectz.combins <- combinations(13, 3, v=objectz, set=TRUE, repeats.allowed=F)
 objectz.combins
 
 
