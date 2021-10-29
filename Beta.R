8ca2370fb133549afdb144c46ac9ff8c37f1b49a
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2021-10-29 07:06:18 +0300

    Add files via upload

---
+++ Beta.R
@@ -0,0 +1,8 @@
+#dbeta(x,alpha, beta)
+#pbeta(x,alpha, beta)
+#qbeta(Fx,alpha, beta)
+#rbeta(numSims, alpha, beta)
+
+# Considering a Beta random variable X with parameter ?? = 3 and ?? = 4.
+# we can find say P(X < 0.3).
+pbeta(0.3, 3, 4)
