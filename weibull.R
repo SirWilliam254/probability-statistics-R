20da7b1a18746ab6ebb9ccc74af8428313ccd411
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2021-10-29 07:05:11 +0300

    Add files via upload

---
+++ weibull.R
@@ -0,0 +1,10 @@
+#dweibull(x, shape = alpha, scale = theta)
+#pweibull(x, shape = alpha, scale = theta)
+#qweibull(Fx, shape = alpha, scale = theta)
+#rweibull(numSims, shape = alpha, scale = theta)
+
+# Lets say we have a Weibull random variable X with shape parameter ?? = 2 and
+# scale parameter ?? = 3. Find P(X < 4.5).
+pweibull(4.5, 2, 3)
+# Alternatively
+1 - exp(-(4.5/3)^2)
