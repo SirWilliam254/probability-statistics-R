65fda4efc3c502a7d3f0b269e35b4d64082a47df
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2021-10-29 07:07:51 +0300

    Add files via upload

---
+++ uniform.R
@@ -0,0 +1,10 @@
+#dunif(x, min, max)
+#punif(x, min, max)
+#qunif(Fx, min, max)
+#runif(numSims, min, max)
+
+# for instance
+# Letting X ??? U(0, 3). we can find P(X ??? 0.5)
+(0.5-0)/(3-0)
+# Alternatively
+punif(0.5, 0, 3)
