aeff22e2e6d7eaa0d3fbddac91c456f9e94836ad
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2021-10-30 07:55:17 +0300

    Update exponential.R

--- exponential.R
+++ exponential.R
@@ -3,8 +3,17 @@
 #qexp(Fx, lambda)
 #rexp(numSims, lambda)
 
-# for instance  an exponential random variable X with mean 3. we can find P(X > 3).
+# for instance  an exponential random variable X with mean 3. we can find P(X > 4).
 lambda <- 1/3
-1 - (1 - exp(-lambda*3))
+1 - (1 - exp(-lambda*4))
 # Alternatively
-1 - pexp(3, lambda)
+1 - pexp(4, lambda)
+
+###  It will fall between 2 and 3
+pexp(3, lambda) - pexp(2, lambda)
+
+###  will be no more than 2 i.e P(X <= 2)
+pexp(2, lambda)
+
+### getting x for prob 0.1
+qexp(0.9, lambda)
