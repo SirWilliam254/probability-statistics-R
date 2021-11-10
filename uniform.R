05ebe6e041f790476ffcf2a5e36361d08cc78f04
William Mburu <69644126+SirWilliam254@users.noreply.github.com>
2021-11-10 07:40:32 +0300

    Update uniform.R

--- uniform.R
+++ uniform.R
@@ -1,7 +1,7 @@
-#dunif(x, min, max)
-#punif(x, min, max)
-#qunif(Fx, min, max)
-#runif(numSims, min, max)
+dunif(x, min, max)
+punif(x, min, max)
+qunif(Fx, min, max)
+runif(numSims, min, max)
 
 # for instance
 # Letting X ??? U(0, 3). we can find P(X ??? 0.5)
